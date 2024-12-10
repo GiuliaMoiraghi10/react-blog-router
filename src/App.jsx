4130681ca7ab3952bbc531ed1b7846c05c4a1887
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2024-12-10 15:46:16 +0100

    Creazione rotta dinamica id - Show

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import HomePage from './Pages/HomePage.jsx'
 import About from './Pages/About.jsx'
 import Contact from './Pages/Contact.jsx'
 import DefaultLayout from './Layout/DefaultLayout.jsx'
+import Show from './Pages/SecondPages/Show.jsx'
 
 function App() {
 
@@ -13,7 +14,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route element={<DefaultLayout />}>
-          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/" element={<HomePage />}>
+            <Route path=":id" element={<Show />}></Route>
+          </Route>
           <Route path="/about" element={<About />}></Route>
           <Route path="/contact" element={<Contact />}></Route>
         </Route>
