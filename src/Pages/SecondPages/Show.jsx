4fc109e021e9e5050abe61d17af6d678d78c7429
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2024-12-11 16:33:21 +0100

    Implementazione pagina Show per ricerca id personaggio

--- src/Pages/SecondPages/Show.jsx
+++ src/Pages/SecondPages/Show.jsx
@@ -1,5 +1,27 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { BASE_API_URI } from '../../baseUrl'
+
 export default function Show() {
+
+    const { id } = useParams()
+    const [characters, setCharacters] = useState(null)
+
+    useEffect(() => {
+        axios.get(`${BASE_API_URI}/characters/${id}`)
+            .then(res => {
+                console.log(res.character);
+                setPosts(res.characters);
+            })
+            .catch(err => {
+                console.error(err);
+                setPosts(null);
+            });
+    }, [id]);
+
     return (
-        <h1>Pagina con dettaglio post</h1>
+        <h1>Pagina con dettaglio personaggio {id}</h1>
+
     )
 }
